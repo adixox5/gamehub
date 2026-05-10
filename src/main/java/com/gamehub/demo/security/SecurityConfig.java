6a91b77ad4501448b0549140912b93e0a3806f2f
Vapoxu <wikglo55@gmail.com>
2026-05-10 21:56:04 +0200

    random gra sec dostęp

--- src/main/java/com/gamehub/demo/security/SecurityConfig.java
+++ src/main/java/com/gamehub/demo/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                         .requestMatchers("/style/**", "/js/**", "/css/**", "/images/**", "/games/**", "/static/**").permitAll()
                         .requestMatchers("/", "/index", "/index.html").permitAll()
                         .requestMatchers("/info", "/info.html", "/regulamin", "/regulamin.html").permitAll()
-                        .requestMatchers("/game", "/game.html").permitAll()
+                        .requestMatchers("/game", "/game.html", "/random-game").permitAll()
                         .requestMatchers("/category", "/category.html").permitAll()
                         .requestMatchers("/api/records/**", "/api/games/**").permitAll()
                         .requestMatchers("/login", "/login.html", "/register", "/register.html").permitAll()
